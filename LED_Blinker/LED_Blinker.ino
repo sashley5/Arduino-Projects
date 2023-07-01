a33816c743d919e98ed3b3114e8ba243b6421e5e
Sarah Ashley <137674008+sashley5@users.noreply.github.com>
2023-07-01 15:33:11 +1000

    Add files via upload

--- LED_Blinker/LED_Blinker.ino
+++ LED_Blinker/LED_Blinker.ino
@@ -1,12 +1,28 @@
+const int SwitchInputPin = 2;
+
 void setup() {
-  // put your setup code here, to run once:
-pinMode(LED_BUILTIN,OUTPUT);
+  // initialize digital pin for the LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  // and similarly the switch as an input
+  pinMode(SwitchInputPin, INPUT);
 }
+//A variable to hold the value of the button - High (TRUE) or Low (FALSE)
+boolean buttonValue;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(LED_BUILTIN,HIGH);
-delay(1000);
-digitalWrite(LED_BUILTIN,LOW);
-delay(1000);
+  // First read the state of the button
+  buttonValue = digitalRead (SwitchInputPin);
+  // Now decide what to do
+  if (buttonValue)
+  {
+    digitalWrite(LED_BUILTIN,HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN,LOW);
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN,LOW);
+  }
 }
+
